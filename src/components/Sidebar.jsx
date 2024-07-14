d40321d63ebf156f55d5496b655afd8567b45473
akash-falcon1999 <carismaticoakash007@gmail.com>
2024-07-15 04:08:51 +0530

    uichanges_search_navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,34 +1,36 @@
-import React from 'react'
-import {Stack} from '@mui/material';
-import { categories } from '../utils/constants';
-import { style } from '@mui/system';
+import React from "react";
+import { Stack } from "@mui/material";
 
+import { categories } from "../utils/constants";
 
-const Sidebar = ({selectedcategory,setselectedCategory}) => {
-  return (
-    <Stack direction='row'
+const Categories = ({ selectedCategory, setSelectedCategory }) => (
+  <Stack
+    direction="row"
     sx={{
-        overflowY:'auto',
-        height:{sx:'auto',md:'95%'},
-        flexDirection:{md:'column'},
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
     }}
-    >
-    {categories.map((category)=>(
-        <button className="category-btn"
-        onClick={()=>setselectedCategory(category.name)}
+  >
+    {categories.map((category) => (
+      <button
+        className="category-btn"
+        onClick={() => setSelectedCategory(category.name)}
         style={{
-            background: category.name===selectedcategory 
-            && 'FC1503',
-            color:'white'
+          background: category.name === selectedCategory && "#FC1503",
+          color: "white",
         }}
         key={category.name}
-        >
-            <span style={{color:category.name===selectedcategory?'white':'blue', marginRight:'15px'}}>{category.icon}</span>
-            <span style={{opacity:category.name===selectedcategory?'1':'0.8'}}>{category.name}</span>
-        </button>
+      >
+        <span style={{ color: category.name === selectedCategory ? "white" : "red", marginRight: "15px" }}>
+          {category.icon}
+        </span>
+        <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>
+          {category.name}
+        </span>
+      </button>
     ))}
-    </Stack>  
-  )
-}
+  </Stack>
+);
 
-export default Sidebar
+export default Categories;
